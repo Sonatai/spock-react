0e9252e3c30304fdafa4ccb65128af2a4a91d937
Sonatai <gamedevsonatai@yahoo.com>
2022-07-31 00:58:21 +0200

    integrate client side routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Start } from './pages/Start/Start';
+import './index.css';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
+import { App } from './App';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+	<React.StrictMode>
+		<BrowserRouter basename='/test-github-pages'>
+			<App />
+		</BrowserRouter>
+	</React.StrictMode>
+);
---
+++ src/pages/Testerino/Testerino.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const Testerino = (): JSX.Element => {
+	return <div>Testerino</div>;
+};
