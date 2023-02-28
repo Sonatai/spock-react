61ae5ec827c704c7464715370384b5348948ae62
Sonatai <gamedevsonatai@yahoo.com>
2023-03-01 00:12:40 +0100

    rename the root

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/test-github-pages/',
+    base: '/spock-react/',
     plugins: [react()],
 });
