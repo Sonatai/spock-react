87eae125fce37306a432bd649e5a662a0ce35c1a
Sonatai <gebauerjenny@yahoo.de>
2023-04-20 22:07:16 +0200

    fix layout issues, clean design #8

--- src/components/shared/Container/Container.tsx
+++
@@ -1,9 +0,0 @@
-import './styles.css';
-
-import { PropsWithChildren } from 'react';
-
-export const Container = (props: PropsWithChildren): JSX.Element => {
-    const { children } = props;
-
-    return <main className="container">{children}</main>;
-};
--- src/components/shared/Layout/Layout.tsx
+++ src/components/shared/Layout/Layout.tsx
@@ -1,29 +1,40 @@
 import './styles.css';
 
+import { nanoid } from 'nanoid';
 import { PropsWithChildren } from 'react';
 
+import { IFeature } from '../../../Hooks/useGetSpec';
+
 interface ILayout {
-    onPageNav?: JSX.Element;
+    hasOnPageNav?: boolean;
+    features?: IFeature[];
 }
 
 export const Layout = (props: PropsWithChildren<ILayout>): JSX.Element => {
-    const { onPageNav, children } = props;
+    const { hasOnPageNav, children, features } = props;
 
     return (
-        <>
-            <div
-                className={`${
-                    onPageNav !== null ? 'w-8/12' : 'w-10/12'
-                } layout-wrapper `}
-            >
+        <div className="lg:pl-[19.5rem]">
+            <div className="max-w-3xl mx-auto pt-10 xl:max-w-none xl:ml-0 xl:mr-[15.5rem] xl:pr-16">
                 {children}
+
+                {hasOnPageNav !== undefined && hasOnPageNav && (
+                    <div className="fixed z-20 top-[3.8125rem] bottom-0 right-[max(0px,calc(50%-45rem))] w-[19.5rem] py-10 overflow-y-auto hidden xl:block">
+                        <h5>On the Page</h5>
+                        <nav>
+                            <ul className="text-[0.875rem]">
+                                {features?.map((feature: IFeature) => (
+                                    <li className="pb-[0.5rem]" key={nanoid()}>
+                                        <a href={`#${feature.id}`}>
+                                            {feature.id}
+                                        </a>
+                                    </li>
+                                ))}
+                            </ul>
+                        </nav>
+                    </div>
+                )}
             </div>
-            {onPageNav !== null && (
-                <div className={'sideNavBar w-2/12'}>
-                    <h5>On the Page</h5>
-                    {onPageNav}
-                </div>
-            )}
-        </>
+        </div>
     );
 };
