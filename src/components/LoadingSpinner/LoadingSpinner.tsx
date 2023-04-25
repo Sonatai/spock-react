c51d0f355b9639fab92af86a6e4299e872ceb3aa
Sonatai <gebauerjenny@yahoo.de>
2023-04-25 10:17:14 +0200

    create loading spinner #28

---
+++ src/components/LoadingSpinner/LoadingSpinner.tsx
@@ -0,0 +1,28 @@
+import './styles.css';
+
+import { faSpinner } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+interface ILoadingSpinner {
+    isLoading: boolean;
+}
+
+export const LoadingSpinner = (props: ILoadingSpinner): JSX.Element => {
+    const { isLoading } = props;
+
+    return (
+        <>
+            {isLoading && (
+                <div id="backdrop">
+                    <div className="text-center">
+                        <FontAwesomeIcon
+                            icon={faSpinner}
+                            spin
+                            className=" spinner-border"
+                        />
+                    </div>
+                </div>
+            )}
+        </>
+    );
+};
