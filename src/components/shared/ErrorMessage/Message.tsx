4c727767cb9ab0d97e39001c92328ad57b16908e
Sonatai <gebauerjenny@yahoo.de>
2023-06-01 20:16:58 +0200

    design message box, impl error boundries around app #27

--- src/components/shared/ErrorMessage/Message.tsx
+++ src/components/shared/ErrorMessage/Message.tsx
@@ -1,20 +1,45 @@
 import './styles.css';
 
-import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import {
+    faCircleCheck,
+    faCircleInfo,
+    faCircleXmark,
+    faTriangleExclamation,
+} from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { ReactNode } from 'react';
+
+type TLevel = 'error' | 'success' | 'info' | 'warning';
 
 interface IErrorMessage {
     headline: string;
-    level?: 'Error' | 'Success' | 'Info' | 'Warning';
+    level: TLevel;
+    children: ReactNode;
 }
 
-export const Message = (props: IErrorMessage) => {
-    const { headline } = props;
+export const Message = (props: IErrorMessage): JSX.Element => {
+    const { headline, level, children } = props;
 
     return (
-        <div className="error__message">
-            <FontAwesomeIcon icon={faMagnifyingGlass} className="" />
-            {headline}
+        <div className="message">
+            <div className={`message__header message__${level}`}>
+                <FontAwesomeIcon icon={icon(level)} className="message__icon" />
+                {headline}
+            </div>
+            <div className="message__content">{children}</div>
         </div>
     );
+};
+
+const icon = (level: TLevel) => {
+    switch (level) {
+        case 'error':
+            return faCircleXmark;
+        case 'info':
+            return faCircleInfo;
+        case 'success':
+            return faCircleCheck;
+        case 'warning':
+            return faTriangleExclamation;
+    }
 };
