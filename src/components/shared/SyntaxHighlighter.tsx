16e2be4233f69f5e483b4fda41339680f34681dd
Jenny <gebauerjenny@yahoo.de>
2023-12-18 20:07:02 +0100

    #110 change to more user friendly theme (#149)

--- src/components/shared/SyntaxHighlighter.tsx
+++ src/components/shared/SyntaxHighlighter.tsx
@@ -1,16 +1,23 @@
 import { Prism } from 'react-syntax-highlighter';
-import { dracula } from 'react-syntax-highlighter/dist/esm/styles/prism';
+import { gruvboxDark } from 'react-syntax-highlighter/dist/esm/styles/prism';
 
 interface ISyntaxHighlighter {
     code: string;
     language: string;
 }
 
+/**
+ * Have no accessibility problems:
+ * a11yDark
+ * coldarkDark
+ * gruvboxDark
+ */
+
 export const SyntaxHighlighter = (props: ISyntaxHighlighter): JSX.Element => {
     const { code, language } = props;
 
     return (
-        <Prism language={language} style={dracula} showLineNumbers>
+        <Prism language={language} style={gruvboxDark} showLineNumbers>
             {code.replace(/\n$/, '')}
         </Prism>
     );
